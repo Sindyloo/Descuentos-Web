5a41f6122857b06ebfbc12204f30487eaf64432c
Sindyloo <87151064+Sindyloo@users.noreply.github.com>
2025-03-24 10:21:42 -0500

    Update Program.cs

--- DescuentosWeb/Program.cs
+++ DescuentosWeb/Program.cs
@@ -20,7 +20,7 @@ namespace DescuentosWeb
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+                    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
                     webBuilder.UseStartup<Startup>()
                               .UseUrls($"http://*:{port}");
                 });
